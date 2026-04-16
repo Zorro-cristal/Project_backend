1a4a8b013d565a2a96145699fea76a3d902321d1
Alejandro Alvarez <zorro201021@hotmail.com>
2026-04-16 15:48:49 -0300

    Correccion en cache realizada

--- src/shell/adaptadores/externals/openmeteo.py
+++ src/shell/adaptadores/externals/openmeteo.py
@@ -2,13 +2,15 @@ import json
 from datetime import datetime
 
 import openmeteo_requests
+import os
 import requests_cache
 from retry_requests import retry
 
 from src.infraestructura.entidad.clima import Clima
 
 # Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+cache_path = os.path.join("/tmp", "cache.sqlite")
+cache_session = requests_cache.CachedSession(cache_path, expire_after=3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
