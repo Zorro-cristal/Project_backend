016b145943875b7e90a5c585f3cb3961f59306bc
Alejandro Alvarez <zorro201021@hotmail.com>
2026-04-15 23:52:51 -0300

    Rutas implementadas

--- src/infraestructura/entidad/detalle_producto.py
+++ src/infraestructura/entidad/detalle_producto.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
+from typing import Optional
 from src.infraestructura.entidad.precio import Precio
 
 @dataclass(frozen=True)
 class Detalle_producto:
-    cod_barra: int
     unidad_por_lote: int
     color: str
     tamanho: int
-    precios: Precio[]
+    precios: list[Precio]
+    cod_barra: Optional[int]= None
--- src/infraestructura/routes/api.py
+++ src/infraestructura/routes/api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-
 from src.shell.adaptadores.externals.openmeteo import obtenerInformacionClimatica
-from src.infraestructura.routes import usuario
+from src.infraestructura.routes import ( marca, precio, usuario, producto, categoria )
 from src.shell.flujo.prueba.conexion_supabase import conexion_supabase
 
 router = APIRouter()
@@ -16,4 +15,8 @@ async def pruebaClima():
     result = obtenerInformacionClimatica(-25.801843, -56.437743, ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "precipitation", "rain", "showers", "snowfall", "weather_code", "cloud_cover", "pressure_msl", "surface_pressure", "wind_speed_10m", "wind_direction_10m", "wind_gusts_10m"])
     return {"message": result} 
 
-router.include_router(usuario.router, prefix="/usuarios", tags=["Usuarios"])
+router.include_router(usuario.router, prefix="/usuario", tags=["Usuario"])
+router.include_router(producto.router, prefix="/producto", tags=["Producto"])
+router.include_router(categoria.router, prefix="/categoria", tags=["Categoria"])
+router.include_router(marca.router, prefix="/marca", tags=["Marca"])
+router.include_router(precio.router, prefix="/precio", tags=["Precio"])
