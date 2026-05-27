b958c37990dbde1e12cbe9e57a073b20be1efbef
Alejandro Alvarez <zorro201021@hotmail.com>
2026-05-27 20:43:20 -0300

    Correccion tabla detalles

--- src/infraestructura/repositories/detalle_producto_repository.py
+++ src/infraestructura/repositories/detalle_producto_repository.py
@@ -5,11 +5,11 @@ from src.shell.adapters.database.generic_crud import get, insert, update
 from src.shell.utils import prepararPayloadDb
 
 async def obtenerDetalleProducto(filtros= None, limite= 100, offset= 0, columnas= "*"):
-    return await get('detalle_producto', filtros, limite, offset)
+    return await get('detalles_producto', filtros, limite, offset)
 
 async def actualizarDetalleProducto(datos: Union[Detalle_producto, dict], cod_barra: Optional[int] = None):
     payload = prepararPayloadDb(datos)
 
     if cod_barra is None:
-        return await insert('detalle_producto', payload)
-    return await update('detalle_producto', cod_barra, payload, key='cod_barra')
+        return await insert('detalles_producto', payload)
+    return await update('detalles_producto', cod_barra, payload, key='cod_barra')
